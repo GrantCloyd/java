82b3928540c25cd06600791b06a3bb6e1e5005c5
Grant Cloyd <dgcloyd@gmail.com>
2022-08-07 15:55:09 -0400

    paint job challenge

--- ControlFlow/Scanner/src/Main.java
+++ ControlFlow/Scanner/src/Main.java
@@ -95,5 +95,26 @@ public class Main {
         scanner.close();
     }
 
+    public class PaintJob {
+        public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+            if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
+                return -1;
+            }
+            double area = width * height;
+            int areaInBuckets = (int)(Math.ceil(area / areaPerBucket));
+            return areaInBuckets - extraBuckets;
+
+        }
+
+        public static int getBucketCount(double width, double height, double areaPerBucket){
+            return getBucketCount(width, height, areaPerBucket, 0);
+        }
+
+        public static int getBucketCount(double area, double areaPerBucket){
+            return getBucketCount(area, 1, areaPerBucket, 0);
+
+        }
+
+    }
 
 }
