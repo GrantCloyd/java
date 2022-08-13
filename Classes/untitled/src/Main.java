97443c23edcaabe54684c58b8baed556c4ab2e64
Grant Cloyd <dgcloyd@gmail.com>
2022-08-13 15:22:56 -0400

    oop

---
+++ Classes/untitled/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void  main(String[] args){
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+
+        firstCar.setModel("Porsche");
+        System.out.println(firstCar.getModel());
+    }
+}
