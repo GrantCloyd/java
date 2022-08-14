df2a607ee157ddbc1a4959402047cdc6ce51c428
Grant Cloyd <dgcloyd@gmail.com>
2022-08-14 16:54:49 -0400

    add constructor

--- Classes/untitled/src/Main.java
+++ Classes/untitled/src/Main.java
@@ -11,24 +11,28 @@ public class Main {
 //
 //
 
-        BankAccount bankAccountOne = new BankAccount();
-        bankAccountOne.setBalance(2000.00);
-        System.out.println(bankAccountOne.setPhoneNumber("222-234-3030"));
-        System.out.println(bankAccountOne.setPhoneNumber("22-23-3030"));
-        System.out.println(bankAccountOne.setPhoneNumber("2222343030"));
-        System.out.println(bankAccountOne.withdrawFunds(250.00));
-        System.out.println(bankAccountOne.depositFunds(565.55));
-        bankAccountOne.setCustomerName("James B");
-        System.out.println(bankAccountOne.setEmail("james"));
-        System.out.println(bankAccountOne.setEmail("jamesb@gmail.com"));
-        System.out.println(bankAccountOne.setAccountNumber("1234567891111"));
-        System.out.println(bankAccountOne.getBalance());
-        System.out.println(bankAccountOne.withdrawFunds(501.00));
-        System.out.println(bankAccountOne.withdrawFunds(.24));
-        System.out.println(bankAccountOne.setAccountNumber("123456789111"));
-        System.out.println(bankAccountOne.withdrawFunds(5001.00));
-        System.out.println(bankAccountOne.withdrawFunds(501.00));
-        System.out.println(bankAccountOne.withDrawFundsFromAtm(222222));
-        System.out.println(bankAccountOne.withDrawFundsFromAtm(20.00));
+//        BankAccount bankAccountOne = new BankAccount();
+//        bankAccountOne.setBalance(2000.00);
+//        System.out.println(bankAccountOne.setPhoneNumber("222-234-3030"));
+//        System.out.println(bankAccountOne.setPhoneNumber("22-23-3030"));
+//        System.out.println(bankAccountOne.setPhoneNumber("2222343030"));
+//        System.out.println(bankAccountOne.withdrawFunds(250.00));
+//        System.out.println(bankAccountOne.depositFunds(565.55));
+//        bankAccountOne.setCustomerName("James B");
+//        System.out.println(bankAccountOne.setEmail("james"));
+//        System.out.println(bankAccountOne.setEmail("jamesb@gmail.com"));
+//        System.out.println(bankAccountOne.setAccountNumber("1234567891111"));
+//        System.out.println(bankAccountOne.getBalance());
+//        System.out.println(bankAccountOne.withdrawFunds(501.00));
+//        System.out.println(bankAccountOne.withdrawFunds(.24));
+//        System.out.println(bankAccountOne.setAccountNumber("123456789111"));
+//        System.out.println(bankAccountOne.withdrawFunds(5001.00));
+//        System.out.println(bankAccountOne.withdrawFunds(501.00));
+//        System.out.println(bankAccountOne.withDrawFundsFromAtm(222222));
+//        System.out.println(bankAccountOne.withDrawFundsFromAtm(20.00));
+        BankAccount bankAccountTwo = new BankAccount("123456789234", 350.00, "Donny P", "atax@net.com", "2404458956");
+        System.out.println(bankAccountTwo.getBalance());
+        System.out.println(bankAccountTwo.getAccountNumber());
+        System.out.println(bankAccountTwo.getCustomerName());
     }
 }
